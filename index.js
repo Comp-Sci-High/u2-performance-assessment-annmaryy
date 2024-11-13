f7c307cd0db30824be4a3d2774ab345b1b5526d5
annmaryy <annmary.quinones25@compscihigh.org>
2024-11-13 15:46:28 +0000

    Added a bit of code :)

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+async function fetchTrip(){
+    const response = await fetch(""+)
 
+}
+
+async function getRandomPlace(count){
+    const response = await fetch(""+ count)
+
+}
+
+
+if (choice === ){
+    const tripName = prompt("Where would you like to travel to? ")
+    await fetchTrip(tripName);
+}
+
+
+aysnc function main() {
+    console.log('Welcome to Trip Advisor Chatbot!');
+    let isRunning = true;
+
+    while(isRunning){
+        console.log('What would you like to do?');
+        console.log('1. Search for a place to travel to');
+        console.log('2. Are you staying for vaction or sight seeing');
+        console.log('3. If you are planning to go sight seeing,');
+        console.log('4. If you are planning to go for vaction,');
+
+
+
+    }
+}
